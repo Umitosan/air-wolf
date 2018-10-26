ded161919bad61394f2fc15e729dcc9225080b28
Umitosan <yeahdom@gmail.com>
2018-10-25 21:03:52 -0700

    style wolf

--- scripts/wolf.js
+++ scripts/wolf.js
@@ -40,32 +40,43 @@ function Wolf() {
     } else {
       console.log('no dir for myWolf.updateVel');
     }
-    console.log("xVel: "+this.xVel+"  yVel: "+this.yVel);
+    // console.log("xVel: "+this.xVel+"  yVel: "+this.yVel);
   };
 
   this.draw = function() {
     // CTX.drawImage(img, dx, dy, dWidth, dHeight);
     // draw wolf
     CTX.drawImage(this.img, this.x-40, this.y-40, 80, 80);
-    // draw propeller center
-    // CTX.save();
-    // CTX.translate(this.propX,this.propY);
-    // CTX.beginPath();
-    // CTX.fillStyle = "lightblue";
-    // CTX.rect(0,0,4,4);
-    // CTX.fill();
-    // CTX.restore();
     // draw propeller
     CTX.save();
     CTX.translate(this.propX+1.5,this.propY+2);
     CTX.rotate(getRadianAngle(this.propAngle));
     CTX.beginPath();
     CTX.strokeStyle = "grey";
-    CTX.lineWidth = 4;
+    CTX.lineWidth = 2;
     CTX.lineCap = "round";
-    CTX.moveTo(-45,0);
-    CTX.lineTo(45,0);
+    CTX.moveTo(-45,2);
+    CTX.lineTo(-45,0);
+    CTX.lineTo(40,0);
+    CTX.lineTo(40,-2);
+    CTX.lineTo(-45,2);
     CTX.stroke();
+    CTX.rotate(getRadianAngle(90));
+    CTX.beginPath();
+    CTX.moveTo(-45,2);
+    CTX.lineTo(-45,0);
+    CTX.lineTo(40,0);
+    CTX.lineTo(40,-2);
+    CTX.lineTo(-45,2);
+    CTX.stroke();
+    CTX.restore();
+    // draw propeller center
+    CTX.save();
+    CTX.translate(this.propX,this.propY);
+    CTX.beginPath();
+    CTX.fillStyle = "black";
+    CTX.rect(0,0,4,4);
+    CTX.fill();
     CTX.restore();
   }; // draw
 
@@ -76,8 +87,8 @@ function Wolf() {
       this.propX = this.x-1.5;
       this.propY = this.y-12;
     }
-    this.propAngle += 38;
-    if (this.propAngle === 38000) { // reset when it gets large
+    this.propAngle += 25;
+    if (this.propAngle === 25000) { // reset when it gets large
       this.propAngle = 0;
     }
   }; // update
