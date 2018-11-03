e47866de7a26e91da2323581dcb625915bea001d
Umitosan <yeahdom@gmail.com>
2018-11-02 19:16:33 -0700

    add mob directional facing

--- scripts/mob.js
+++ scripts/mob.js
@@ -3,9 +3,12 @@
 function Mob(nX,nY,nXvel,nYvel) {
   this.x = nX;
   this.y = nY;
-  this.size = 24;
+  this.size = 28;
   this.xVel = nXvel;
   this.yVel = nYvel;
+  this.img = undefined;
+  this.imgSrc = undefined;
+  this.imgFacing = undefined;
   this.timeBorn = undefined;
   this.dead = undefined;
   this.timeDeath = undefined;
@@ -16,6 +19,20 @@ function Mob(nX,nY,nXvel,nYvel) {
     this.timeBorn = performance.now();
     this.dead = false;
     this.destroyMe = false;
+    this.img = new Image();
+    this.imgSrc = 'images/snail.png';
+    this.img.src = this.imgSrc;
+    this.updateFacingDir();
+  };
+
+  this.updateFacingDir = function() {
+    if (this.xVel > 0) {
+      this.imgFacing = 'right';
+    } else if (this.xVel < 0) {
+      this.imgFacing = 'left';
+    } else {
+      // nothin
+    }
   };
 
   this.stop = function() {
@@ -47,7 +64,7 @@ function Mob(nX,nY,nXvel,nYvel) {
       CTX.translate(this.x+(this.size/2),this.y+(this.size/2));
       CTX.beginPath();
       CTX.lineWidth = 2;
-      CTX.strokeStyle = 'red';
+      CTX.strokeStyle = 'brown';
       for (let i = 0; i < 10; i++) {
         CTX.rotate(getRadianAngle(35));
         CTX.moveTo(10,0);
@@ -56,18 +73,29 @@ function Mob(nX,nY,nXvel,nYvel) {
       CTX.stroke();
       CTX.restore();
     } else {
-      CTX.beginPath();
-      CTX.lineWidth = 1;
-      CTX.strokeStyle = 'purple';
-      CTX.fillStyle = 'purple';
-      CTX.rect(this.x,this.y,this.size,this.size);
-      CTX.stroke();
-      CTX.fill();
+      // hit box
+      // CTX.beginPath();
+      // CTX.lineWidth = 1;
+      // CTX.strokeStyle = 'purple';
+      // CTX.rect(this.x,this.y,this.size,this.size);
+      // CTX.stroke();
+      if (this.imgFacing === 'right') {
+        CTX.save();
+        CTX.translate(this.x+(this.size/2), this.y+(this.size/2));
+        CTX.scale(-1,1);
+        CTX.drawImage(this.img, 0-(this.size/2), 0-(this.size/2), this.size, this.size); // CTX.drawImage(img, dx, dy, dWidth, dHeight);
+        CTX.restore();
+      } else { // already facing left by default
+        CTX.drawImage(this.img, this.x, this.y, this.size, this.size);
+      }
     }
   };
 
   this.update = function() {
-    if (this.hitWall()) { this.xVel *= -1; }
+    if (this.hitWall()) {
+      this.xVel *= -1;
+      this.updateFacingDir();
+    }
     if (this.dead === true) {
       if ((performance.now() - this.timeDeath) > this.timeDeathDur) {
         this.destroyMe = true;
